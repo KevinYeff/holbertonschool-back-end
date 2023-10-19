1a585b13080f83e5f70752c9782898e64c669983
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 17:23:27 -0500

    fix: file meets pep8 requirements

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,20 +12,23 @@ NUMBER_OF_DONE_TASKS = 0
 TOTAL_NUMBER_OF_TASKS = 0
 TASK_TITLE = []
 
+
 def get_api_response():
     """This function will gather and print data from a API"""
-    global EMPLOYEE_NAME, EMPLOYEE_ID, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS, TASK_TITLE
+    global EMPLOYEE_NAME, EMPLOYEE_ID, NUMBER_OF_DONE_TASKS
+    global TOTAL_NUMBER_OF_TASKS, TASK_TITLE
     # request users
     user_request = requests.get('https://jsonplaceholder.typicode.com/users')
     # request todos
     todos_request = requests.get('https://jsonplaceholder.typicode.com/todos')
     # extract user id that matches employee id to extract the name
     EMPLOYEE_ID = int(argv[1])
+
     for user in user_request.json():
         if user['id'] == EMPLOYEE_ID:
             EMPLOYEE_NAME = user['name']
             break
-        
+
     # extract the task completed
     for task in todos_request.json():
         if task['userId'] == EMPLOYEE_ID:
@@ -40,7 +43,7 @@ def get_api_response():
     # print tab + todos completed
     for title in TASK_TITLE:
         print('\t {}'.format(title))
-    
+
 # make sure script doesn't execute when imported
 if __name__ == '__main__':
     get_api_response()
