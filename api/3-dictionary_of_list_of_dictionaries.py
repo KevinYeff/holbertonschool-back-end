bc028ddf5688fe26aeffd34962582ddab7f25e70
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 17:42:06 -0500

    fix: file now meets pep8 requirements

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -2,10 +2,10 @@
 """Script that uses a rest API and request for a given
 employee ID and returns information about his/her TODO
 list progress"""
-#libraries
-import requests
+# libraries
 import json
-#using the global variables
+import requests
+# using the global variables
 
 EMPLOYEE_ID = None
 NUMBER_OF_DONE_TASKS = 0
@@ -19,7 +19,7 @@ TASK_COMPLETED_STATUS = ''
 def get_api_response():
     """This function will gather and print data from an API."""
     global EMPLOYEES, TASK_TITLE
-    
+
     # Request users
     user_request = requests.get('https://jsonplaceholder.typicode.com/users')
     # Request todos
@@ -35,7 +35,7 @@ def export_all_to_json(users, tasks):
     global EMPLOYEE_ID, NUMBER_OF_DONE_TASKS
     global TOTAL_NUMBER_OF_TASKS, TASK_TITLE, EMPLOYEE_USERNAME
     global TASK_COMPLETED_STATUS
-        
+
     # Creating a dictionary to organize tasks
     all_employee_data = {}
     for user in users:
@@ -46,13 +46,13 @@ def export_all_to_json(users, tasks):
             if task['userId'] == EMPLOYEE_ID:
                 TASK_TITLE = task['title']
                 TASK_COMPLETED_STATUS = task['completed']
-                TOTAL_NUMBER_OF_TASKS.append({"username": EMPLOYEE_USERNAME, 
-                                              "task": TASK_TITLE, 
+                TOTAL_NUMBER_OF_TASKS.append({"username": EMPLOYEE_USERNAME,
+                                              "task": TASK_TITLE,
                                               "completed": TASK_COMPLETED_STATUS})
-                
+
         all_employee_data[EMPLOYEE_ID] = TOTAL_NUMBER_OF_TASKS
-        
-    
+
+
     # Write all the data to a json file
     with open(f'todo_all_employees.json', 'w') as jsonfile:
         json.dump(all_employee_data, jsonfile, indent=4)
