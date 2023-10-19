eec8b5b4b1a94b8a0e8fa8f78157d63c015b94bb
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 17:19:01 -0500

    feat: Python scrpit that gathers all information from a REST API This script prints the information required from all users and export the data to a json format (file)

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Script that uses a rest API and request for a given
+employee ID and returns information about his/her TODO
+list progress"""
+#libraries
+import requests
+import json
+#using the global variables
+
+EMPLOYEE_ID = None
+NUMBER_OF_DONE_TASKS = 0
+TOTAL_NUMBER_OF_TASKS = 0
+TASK_TITLE = []
+EMPLOYEE_USERNAME = ''
+EMPLOYEES = None
+TASK_COMPLETED_STATUS = ''
+
+
+def get_api_response():
+    """This function will gather and print data from an API."""
+    global EMPLOYEES, TASK_TITLE
+    
+    # Request users
+    user_request = requests.get('https://jsonplaceholder.typicode.com/users')
+    # Request todos
+    todos_request = requests.get('https://jsonplaceholder.typicode.com/todos')
+    EMPLOYEES = user_request.json()
+    TASK_TITLE = todos_request.json()
+    # Export all data to JSON
+    export_all_to_json(EMPLOYEES, TASK_TITLE)
+
+
+def export_all_to_json(users, tasks):
+    """This function will export all data to JSON."""
+    global EMPLOYEE_ID, NUMBER_OF_DONE_TASKS
+    global TOTAL_NUMBER_OF_TASKS, TASK_TITLE, EMPLOYEE_USERNAME
+    global TASK_COMPLETED_STATUS
+        
+    # Creating a dictionary to organize tasks
+    all_employee_data = {}
+    for user in users:
+        EMPLOYEE_ID = user['id']
+        EMPLOYEE_USERNAME = user['username']
+        TOTAL_NUMBER_OF_TASKS = []
+        for task in tasks:
+            if task['userId'] == EMPLOYEE_ID:
+                TASK_TITLE = task['title']
+                TASK_COMPLETED_STATUS = task['completed']
+                TOTAL_NUMBER_OF_TASKS.append({"username": EMPLOYEE_USERNAME, 
+                                              "task": TASK_TITLE, 
+                                              "completed": TASK_COMPLETED_STATUS})
+                
+        all_employee_data[EMPLOYEE_ID] = TOTAL_NUMBER_OF_TASKS
+        
+    
+    # Write all the data to a json file
+    with open(f'todo_all_employees.json', 'w') as jsonfile:
+        json.dump(all_employee_data, jsonfile, indent=4)
+
+
+if __name__ == '__main__':
+    # Get the API response
+    get_api_response()
